099d0f1122ab0616cbbd797d7b5af2f2f65f1435
Lorenzo Pucci <lorenzo.pucci5@gmail.com>
2022-08-19 07:29:58 +0200

    Implement item addition to the database

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 var express = require("express");
 var http = require("http");
+var bodyParser = require("body-parser");
+var fs = require("fs");
 //var favicon = require("serve-favicon");
 
 var app = express();
 var server = http.createServer(app);
 //app.use(favicon(__dirname + '/static/images/favicon.ico'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.set('view engine', 'ejs');
 
 server.listen(8000, () => {
@@ -61,6 +65,43 @@ app.get('/search', (req, res) => {
     });
 
     res.render(__dirname + "/pages/index.ejs", {list});
+});
+
+app.get('/add', (req, res) => {
+    res.render(__dirname + "/pages/add.ejs");
+});
+
+app.post('/add/commit', (req, res) => {
+    let data = require("./database.json");
+    let ingredients = [];
+
+    for (let key in req.body) {
+        if (key.startsWith("ingr-") && key.endsWith("-name") &&
+            req.body[key] !== "") {
+            ingredients.push({
+                name: req.body[key],
+                amount: req.body["ingr-" + key.split("-")[1] + "-amount"]
+            });
+        }
+    }
+
+    data.push({
+        id: data[data.length - 1].id + 1,
+        name: req.body.name.charAt(0).toUpperCase() + req.body.name.slice(1),
+        type: req.body.type,
+        difficulty: parseInt(req.body.difficulty),
+        doses_for: parseInt(req.body.doses_for),
+        ingredients: ingredients,
+        procedure: req.body.procedure
+    });
+
+    fs.writeFile(__dirname + "/database.json", 'UTF-8', JSON.stringify(data),
+        (err) => {
+        console.log("Error in writing JSON to database");
+        console.log(err);
+    });
+    
+    res.redirect('/');
 });
 
 app.get('/static/*', (req, res) => {
